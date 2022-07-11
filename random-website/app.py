4e4c5bcf46f82b7c047464e5864a3c5e08d3d622
Richard Miao <richardmiao1111@gmail.com>
2022-07-11 16:51:04 -0400

    update

--- random-website/app.py
+++ random-website/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, flash, url_for
 from flask_session import Session
 from cs50 import SQL
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -9,6 +9,7 @@ db = SQL("sqlite:///accounts.db")
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
@@ -50,13 +51,17 @@ def register():
 
     if request.method == "POST":
         if not request.form.get("username"):
-            return apology("The username field is required! ")
+            flash("The username field is required! ")
+            return render_template("register.html")
         elif not request.form.get("password"): 
-            return apology("The password field is required! ")
+            flash("The password field is required! ")
+            return render_template("register.html")
         elif request.form.get("password") != request.form.get("confirmation"):
-            return apology("The passwords must match! ")
+            flash("The password and password confirmation fields must match! ")
+            return render_template("register.html")
         elif db.execute("SELECT username FROM accounts WHERE username=:username", username=request.form.get("username")):
-            return apology("Username is taken! Please try again.")
+            flash("This username is taken! Please try again.")
+            return render_template("register.html")
         
         username = request.form.get("username")
         password = generate_password_hash(request.form.get("password"), method="pbkdf2:sha256", salt_length=8)
@@ -81,14 +86,17 @@ def login():
         return render_template("login.html")
     
     if not request.form.get("username"):
-        return apology("Username field cannot be blank!")
+        flash("Username field cannot be blank!")
+        return render_template("login.html")
     elif not request.form.get("password"):
-        return apology("Password field cannot be blank!")
+        flash("Password field cannot be blank!")
+        return render_template("login.html")
     
     rows = db.execute("SELECT * FROM accounts WHERE username = ?", request.form.get("username"))
 
     if (len(rows) != 1) or not (check_password_hash(rows[0]["password"], request.form.get("password"))):
-        return apology("Invalid username and/or password.", 403)
+        flash("Invalid username and/or password.")
+        return render_template("login.html")
     
     session["user_id"] = rows[0]["id"]
 
@@ -98,6 +106,7 @@ def login():
 @app.route("/logout", methods = ["GET", "POST"])
 def logout():
     session.clear()
+    flash("Logged out successfully!", "info")
     return redirect("/")
 
 
@@ -127,22 +136,83 @@ def create_task():
 def settings():
     if request.method == "GET":
         return render_template("settings.html")
+
+    account = db.execute("SELECT * FROM accounts WHERE id=:id", id=session["user_id"])
+    if request.form.get("email"):
+        return render_template("change_email.html", account=account)
+    if request.form.get("username"):
+        return render_template("change_username.html", account=account)
+    elif request.form.get("password"):
+        return render_template("change_password.html", account=account)
+    elif request.form.get("delete"):
+        return render_template("delete_account.html", account=account)
+
+
+@app.route("/settings/change_email", methods = ["GET", "POST"])
+@login_required
+def change_email():
+    if request.method == "GET":
+        return render_template("change_email.html")
     
     c_email = request.form.get("email")
-    c_username = request.form.get("changed_username")
-    c_password = request.form.get("changed_password")
-    c_confirmation = request.form.get("changed_password_confirmation")
-    
     if c_email: 
-        db.execute("UPDATE accounts SET email=? WHERE id=?", c_email, session["user_id"])
-    elif c_username:
-        db.execute("UPDATE accounts SET username=? WHERE id=?", c_username, session["user_id"])
-    elif c_password and c_confirmation and (c_password == c_confirmation):
-        db.execute("UPDATE accounts SET password=? WHERE id=?", c_username, session["user_id"])
-
+        db.execute("UPDATE accounts SET email=:email WHERE id=:id", email=c_email, id=session["user_id"])
     return redirect("/settings")
 
 
+@app.route("/settings/change_username", methods = ["GET", "POST"])
+@login_required
+def change_username():
+    if request.method == "GET":
+        return render_template("change_username.html")
+
+    c_username = request.form.get("changed_username")
+    if c_username:
+        db.execute("UPDATE accounts SET username=:username WHERE id=:id", username=c_username, id=session["user_id"])
+    return redirect("/settings")
+
+
+@app.route("/settings/change_password", methods = ["GET", "POST"])
+@login_required
+def change_password():
+    if request.method == "GET":
+        return render_template("change_password.html")
+
+    c_password = request.form.get("changed_password")
+    c_confirmation = request.form.get("changed_password_confirmation")
+    if c_password and c_confirmation and (c_password == c_confirmation):
+        db.execute("UPDATE accounts SET password=? WHERE id=?", c_password, session["user_id"])
+    return redirect("/settings")
+
+@app.route("/settings/delete_account", methods = ["GET", "POST"])
+@login_required
+def delete_account():
+    if request.method == "GET":
+        return render_template("delete_account.html")
+    
+    password = request.form.get("password")
+    password_c = request.form.get("password_confirmation")
+    consent = request.form.get("consent")
+    rows = db.execute("SELECT * FROM accounts WHERE username = ?", request.form.get("username"))
+
+    
+    if not password or not password_c: 
+        flash("Your password(s) is/are required!")
+        return render_template("delete_account.html")
+    elif password != password_c:
+        flash("Your passwords must match!")
+        render_template("delete_account.html")
+    elif (len(rows) != 1) or not (check_password_hash(rows[0]["password"], request.form.get("password"))):
+        flash("Invalid username and/or password.")
+        return render_template("delete_account.html")
+    elif not consent:
+        flash("Consent is required. Please check the box to proceed.")
+        return render_template("delete_account.html")
+    
+    db.execute("DELETE FROM accounts WHERE id=:id", id=session["user_id"])
+    db.execute("DELETE FROM tasks WHERE account_id=:account_id", account_id=session["user_id"])
+    return redirect("/login")
+    
 #if __name__ == "__main__":
   #  app.run()
 
